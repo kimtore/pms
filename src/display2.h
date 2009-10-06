b9e1ae0ace1559931ab9b101ac3666d6a429fea2
Kim Tore Jensen <kimtjen@gmail.com>
2009-10-06 12:08:09 +0200

    Commit stash

--- src/display2.h
+++ src/display2.h
@@ -1,3 +1,14 @@
+/*
+ * T er for eksempel Song, Window, Object, alt mulig
+ */
+
+template <class T>
+class ListView
+{
+	T *		list;
+public:
+	void		select(long pos, int state);
+}
 
 
 class Window
