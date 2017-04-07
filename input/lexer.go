486ec014011bb0fbb4d5c1e9be6605cdfc9f5f17
Kim Tore Jensen <kim@incendio.no>
2017-04-07 14:27:24 +0200

    Clean up tokenizer code

--- input/lexer.go
+++ input/lexer.go
@@ -1,9 +1,12 @@
 package input
 
-import "unicode"
+import (
+	"unicode"
+	"unicode/utf8"
+)
 
 type Token struct {
-	Class string
+	Class int
 	Runes []rune
 }
 
@@ -13,30 +16,32 @@ func NewToken() Token {
 	return t
 }
 
-// Tokenize is a lexer for the PMS input language. It splits up an input sentence into tokens, separating
-func Tokenize(s string) []Token {
-	tokens := make([]Token, 0)
-	t := NewToken()
-	for _, r := range s {
-		if t.Class != "comment" && unicode.IsSpace(r) {
+const (
+	TokenEnd = iota
+	TokenIdentifier
+	TokenComment
+)
+
+// Tokenize is a lexer for the input language. It extracts the next token out of a sentence, and returns
+func NextToken(input string) (t Token, pos int) {
+	t = NewToken()
+	for _, r := range input {
+		pos += utf8.RuneLen(r)
+		if t.Class != TokenComment && unicode.IsSpace(r) {
 			if len(t.Runes) > 0 {
-				tokens = append(tokens, t)
-				t = NewToken()
+				return
 			}
 			continue
 		} else if len(t.Runes) == 0 {
 			switch r {
 			case '#':
 				// Comments terminate the line
-				t.Class = "comment"
+				t.Class = TokenComment
 			default:
-				t.Class = "identifier"
+				t.Class = TokenIdentifier
 			}
 		}
 		t.Runes = append(t.Runes, r)
 	}
-	if len(t.Runes) > 0 {
-		tokens = append(tokens, t)
-	}
-	return tokens
+	return
 }
--- input/lexer_test.go
+++ input/lexer_test.go
@@ -6,25 +6,49 @@ import (
 	"github.com/ambientsound/pms/input"
 )
 
+// TestLexer tests the input.NextToken() function, checking that it correctly
+// splits up input lines into Token structs.
 func TestLexer(t *testing.T) {
+	i := 0
+	pos := 0
 	str := "the  quick <brown>\t \tfoxee # adds a comment"
-	check := []input.Token{
-		input.Token{Runes: []rune("the"), Class: "identifier"},
-		input.Token{Runes: []rune("quick"), Class: "identifier"},
-		input.Token{Runes: []rune("<brown>"), Class: "identifier"},
-		input.Token{Runes: []rune("foxee"), Class: "identifier"},
-		input.Token{Runes: []rune("# adds a comment"), Class: "comment"},
+	token := input.Token{}
+
+	checks := []input.Token{
+		input.Token{Class: input.TokenIdentifier, Runes: []rune("the")},
+		input.Token{Class: input.TokenIdentifier, Runes: []rune("quick")},
+		input.Token{Class: input.TokenIdentifier, Runes: []rune("<brown>")},
+		input.Token{Class: input.TokenIdentifier, Runes: []rune("foxee")},
+		input.Token{Class: input.TokenComment, Runes: []rune("# adds a comment")},
+		input.Token{Class: input.TokenEnd, Runes: nil},
 	}
-	tokens := input.Tokenize(str)
-	if len(check) != len(tokens) {
-		t.FailNow()
-	}
-	for i := range tokens {
-		if tokens[i].Class != check[i].Class {
-			t.FailNow()
+
+	for {
+
+		if i == len(checks) {
+			if token.Class == input.TokenEnd {
+				break
+			}
+			t.Fatalf("Tokenizer generated too many tokens!")
 		}
-		if string(check[i].Runes) != string(tokens[i].Runes) {
-			t.FailNow()
+
+		check := checks[i]
+		token, npos := input.NextToken(str[pos:])
+		pos += npos
+
+		t.Logf("Token %d: pos=%d, runes='%s', input='%s'", i, pos, string(token.Runes), str)
+
+		if token.Class != check.Class {
+			t.Fatalf("Token class for token %d is wrong; expected %d but got %d", i+1, check.Class, token.Class)
 		}
+
+		if string(check.Runes) != string(token.Runes) {
+			t.Fatalf("String check against token %d failed; expected '%s' but got '%s'", i+1,
+				string(check.Runes),
+				string(token.Runes),
+			)
+		}
+
+		i++
 	}
 }
