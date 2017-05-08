c1fbd2c97f62be99134d39588dcbe9f939ba1f1a
Kim Tore Jensen <kim@incendio.no>
2017-05-08 19:35:01 +0200

    Rewrite input lexer test to be table-driven

--- input/lexer/lexer_test.go
+++ input/lexer/lexer_test.go
@@ -6,49 +6,66 @@ import (
 	"github.com/ambientsound/pms/input/lexer"
 )
 
+type result struct {
+	class int
+	str   string
+}
+
+var lexerTests = []struct {
+	input    string
+	expected []result
+}{
+	{
+		"the  quick <brown>\t \tfoxee # adds a comment",
+		[]result{
+			{class: lexer.TokenIdentifier, str: "the"},
+			{class: lexer.TokenIdentifier, str: "quick"},
+			{class: lexer.TokenIdentifier, str: "<brown>"},
+			{class: lexer.TokenIdentifier, str: "foxee"},
+			{class: lexer.TokenComment, str: "# adds a comment"},
+			{class: lexer.TokenEnd, str: ""},
+		},
+	},
+}
+
 // TestLexer tests the lexer.NextToken() function, checking that it correctly
 // splits up input lines into Token structs.
 func TestLexer(t *testing.T) {
-	i := 0
-	pos := 0
-	str := "the  quick <brown>\t \tfoxee # adds a comment"
-	token := lexer.Token{}
+	var token lexer.Token
 
-	checks := []lexer.Token{
-		lexer.Token{Class: lexer.TokenIdentifier, Runes: []rune("the")},
-		lexer.Token{Class: lexer.TokenIdentifier, Runes: []rune("quick")},
-		lexer.Token{Class: lexer.TokenIdentifier, Runes: []rune("<brown>")},
-		lexer.Token{Class: lexer.TokenIdentifier, Runes: []rune("foxee")},
-		lexer.Token{Class: lexer.TokenComment, Runes: []rune("# adds a comment")},
-		lexer.Token{Class: lexer.TokenEnd, Runes: nil},
-	}
+	for _, test := range lexerTests {
 
-	for {
+		i := 0
+		pos := 0
 
-		if i == len(checks) {
-			if token.Class == lexer.TokenEnd {
-				break
+		for {
+
+			if i == len(test.expected) {
+				if token.Class == lexer.TokenEnd {
+					break
+				}
+				t.Fatalf("Tokenizer generated too many tokens!")
 			}
-			t.Fatalf("Tokenizer generated too many tokens!")
+
+			check := test.expected[i]
+			token, npos := lexer.NextToken(test.input[pos:])
+			pos += npos
+			str := token.String()
+
+			t.Logf("Token %d: pos=%d, runes='%s', input='%s'", i, pos, str, test.input)
+
+			if token.Class != check.class {
+				t.Fatalf("Token class for token %d is wrong; expected %d but got %d", i+1, check.class, token.Class)
+			}
+
+			if check.str != str {
+				t.Fatalf("String check against token %d failed; expected '%s' but got '%s'", i+1,
+					check.str,
+					str,
+				)
+			}
+
+			i++
 		}
-
-		check := checks[i]
-		token, npos := lexer.NextToken(str[pos:])
-		pos += npos
-
-		t.Logf("Token %d: pos=%d, runes='%s', input='%s'", i, pos, string(token.Runes), str)
-
-		if token.Class != check.Class {
-			t.Fatalf("Token class for token %d is wrong; expected %d but got %d", i+1, check.Class, token.Class)
-		}
-
-		if string(check.Runes) != string(token.Runes) {
-			t.Fatalf("String check against token %d failed; expected '%s' but got '%s'", i+1,
-				string(check.Runes),
-				string(token.Runes),
-			)
-		}
-
-		i++
 	}
 }
