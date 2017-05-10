48ab990ee5ba720967f80f90fb2a72f5a4105f14
Kim Tore Jensen <kim@incendio.no>
2017-05-10 22:50:24 +0200

    Offer unicode strings in topbar fragment ${state|unicode}

--- topbar/state.go
+++ topbar/state.go
@@ -12,18 +12,30 @@ var stateStrings = map[string]string{
 	mpd.StateUnknown: "??",
 }
 
+var stateUnicodes = map[string]string{
+	mpd.StatePlay:    "\u25b6",
+	mpd.StatePause:   "\u23f8",
+	mpd.StateStop:    "\u23f9",
+	mpd.StateUnknown: "\u2bd1",
+}
+
 // State draws the current player state as an ASCII symbol.
 type State struct {
-	api api.API
+	api   api.API
+	table map[string]string
 }
 
 // NewState returns State.
 func NewState(a api.API, param string) Fragment {
-	return &State{a}
+	table := stateStrings
+	if param == "unicode" {
+		table = stateUnicodes
+	}
+	return &State{a, table}
 }
 
 // Text implements Fragment.
 func (w *State) Text() (string, string) {
 	playerStatus := w.api.PlayerStatus()
-	return stateStrings[playerStatus.State], `state`
+	return w.table[playerStatus.State], `state`
 }
