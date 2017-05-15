80b34f10244b3842a49dd154b16052a1ed4797b1
Kim Tore Jensen <kim@incendio.no>
2017-05-15 22:56:01 +0200

    Improve 'add' command: new parser, ability to add multiple songs at once

--- commands/add.go
+++ commands/add.go
@@ -2,75 +2,107 @@ package commands
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/ambientsound/gompd/mpd"
 	"github.com/ambientsound/pms/api"
 	"github.com/ambientsound/pms/input/lexer"
+	"github.com/ambientsound/pms/parser"
 	"github.com/ambientsound/pms/song"
 	"github.com/ambientsound/pms/songlist"
 )
 
 // Add adds songs to MPD's queue.
 type Add struct {
+	parser.Parser
 	command
 	api      api.API
-	song     *song.Song
 	songlist songlist.Songlist
 }
 
 func NewAdd(api api.API) Command {
 	return &Add{
-		api: api,
+		api:      api,
+		songlist: songlist.New(),
 	}
 }
 
+// FIXME: remove when Scanned() is removed from 'command' struct, and
+// responsibility handed over to parser.Parser
+func (cmd *Add) Scanned() []parser.Token {
+	return cmd.Parser.Scanned()
+}
+
+// Execute implements Command.Execute.
+// FIXME: boilerplate until Execute is removed from interface
 func (cmd *Add) Execute(class int, s string) error {
-	var err error
+	if class == lexer.TokenEnd {
+		return cmd.Exec()
+	}
+	cmd.cmdline += " " + s
+	return nil
+}
 
-	switch class {
-	case lexer.TokenIdentifier:
-		if cmd.song != nil {
-			return fmt.Errorf("Cannot add multiple paths on the same command line.")
-		}
-		cmd.song = song.New()
-		cmd.song.SetTags(mpd.Attrs{
-			"file": s,
-		})
-
-	case lexer.TokenEnd:
-		list := cmd.api.SonglistWidget().Songlist()
-		queue := cmd.api.Queue()
-
-		switch {
-		case cmd.song == nil:
-			selection := list.Selection()
-			if selection.Len() == 0 {
-				return fmt.Errorf("No selection, cannot add without any parameters.")
-			}
-			err = queue.AddList(selection)
-			if err != nil {
-				break
-			}
-			list.ClearSelection()
-			list.MoveCursor(1)
-			len := selection.Len()
-			if len == 1 {
-				song := selection.Songs()[0]
-				cmd.api.Message("Added to queue: %s", song.StringTags["file"])
-			} else {
-				cmd.api.Message("Added %d songs to queue.", len)
-			}
-
-		default:
-			err = queue.Add(cmd.song)
-			if err == nil {
-				cmd.api.Message("Added to queue: %s", cmd.song.StringTags["file"])
-			}
-		}
-
-	default:
-		return fmt.Errorf("Unknown input '%s', expected END", s)
+// Exec is the next Execute(), evading the old system
+func (cmd *Add) Exec() error {
+	// FIXME: move this code out of Command
+	reader := strings.NewReader(cmd.cmdline)
+	scanner := lexer.NewScanner(reader)
+	err := cmd.Parse(scanner)
+	if err != nil {
+		return err
 	}
 
-	return err
+	list := cmd.api.SonglistWidget().Songlist()
+	queue := cmd.api.Queue()
+
+	err = queue.AddList(cmd.songlist)
+	if err != nil {
+		return err
+	}
+
+	list.ClearSelection()
+	list.MoveCursor(1)
+	len := cmd.songlist.Len()
+	if len == 1 {
+		song := cmd.songlist.Songs()[0]
+		cmd.api.Message("Added to queue: %s", song.StringTags["file"])
+	} else {
+		cmd.api.Message("Added %d songs to queue.", len)
+	}
+
+	return nil
+}
+
+// Parse implements Command.
+func (cmd *Add) Parse(s *lexer.Scanner) error {
+
+	// FIXME: initial verb scan boilerplate?
+	cmd.S = s
+
+	// Add all songs specified
+Loop:
+	for {
+		tok, lit := cmd.ScanIgnoreWhitespace()
+		switch tok {
+		case lexer.TokenIdentifier:
+		case lexer.TokenWhitespace, lexer.TokenEnd:
+			break Loop
+		default:
+			return fmt.Errorf("Unexpected %v, expected identifier", lit)
+		}
+		addSong := song.New()
+		addSong.SetTags(mpd.Attrs{"file": lit})
+		cmd.songlist.Add(addSong)
+	}
+
+	// No songs specified on command line. Use songlist selection instead.
+	if cmd.songlist.Len() == 0 {
+		cmd.songlist = cmd.api.Songlist().Selection()
+		if cmd.songlist.Len() == 0 {
+			return fmt.Errorf("No selection, cannot add without any parameters.")
+		}
+	}
+
+	return nil
 }
