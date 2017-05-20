f0fb4e15b2ee6acd9cb46e74b70cd61b8dfe98e2
Kim Tore Jensen <kim@incendio.no>
2017-05-20 21:43:38 +0200

    Reduce cyclomatic complexity in Style::Parse()

--- commands/style.go
+++ commands/style.go
@@ -50,39 +50,50 @@ func (cmd *Style) Parse() error {
 			return fmt.Errorf("Unexpected '%v', expected identifier", lit)
 		}
 
-		switch lit {
-		case "blink":
-			cmd.styleValue = cmd.styleValue.Blink(true)
-		case "bold":
-			cmd.styleValue = cmd.styleValue.Bold(true)
-		case "dim":
-			cmd.styleValue = cmd.styleValue.Dim(true)
-		case "reverse":
-			cmd.styleValue = cmd.styleValue.Reverse(true)
-		case "underline":
-			cmd.styleValue = cmd.styleValue.Underline(true)
-		default:
-			if lit[0] == '@' {
-				lit = "#" + lit[1:]
-			}
-			color := tcell.GetColor(lit)
-			switch {
-			case !cmd.foreground:
-				cmd.styleValue = cmd.styleValue.Foreground(color)
-				cmd.foreground = true
-			case !cmd.background:
-				cmd.styleValue = cmd.styleValue.Background(color)
-				cmd.background = true
-			default:
-				return fmt.Errorf("Only two color values are allowed per style.")
-			}
+		err := cmd.mergeStyle(lit)
+		if err != nil {
+			return err
 		}
 	}
+
+	return nil
 }
 
 // Exec implements Command.
 func (cmd *Style) Exec() error {
 	styleMap := cmd.api.Styles()
 	styleMap[cmd.styleKey] = cmd.styleValue
+	return nil
+}
+
+func (cmd *Style) mergeStyle(lit string) error {
+	switch lit {
+	case "blink":
+		cmd.styleValue = cmd.styleValue.Blink(true)
+	case "bold":
+		cmd.styleValue = cmd.styleValue.Bold(true)
+	case "dim":
+		cmd.styleValue = cmd.styleValue.Dim(true)
+	case "reverse":
+		cmd.styleValue = cmd.styleValue.Reverse(true)
+	case "underline":
+		cmd.styleValue = cmd.styleValue.Underline(true)
+	default:
+		if lit[0] == '@' {
+			lit = "#" + lit[1:]
+		}
+		color := tcell.GetColor(lit)
+		switch {
+		case !cmd.foreground:
+			cmd.styleValue = cmd.styleValue.Foreground(color)
+			cmd.foreground = true
+		case !cmd.background:
+			cmd.styleValue = cmd.styleValue.Background(color)
+			cmd.background = true
+		default:
+			return fmt.Errorf("Only two color values are allowed per style.")
+		}
+	}
+
 	return nil
 }
